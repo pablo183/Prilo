5f490bd58820c4eb5702dbdfce60af5626d08c77
pablo183 <pawelkrzynka@gmail.com>
2023-05-18 20:37:11 +0200

    Improving Login Shipper

--- src/test/java/stepDefinitions/LoginShipper.java
+++ src/test/java/stepDefinitions/LoginShipper.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -18,13 +19,6 @@ import static com.codeborne.selenide.Condition.*;
 
 public class LoginShipper {
 
-    /*
-    @Given("The login shipper")
-    public void the_login_shipper() {
-        the_user_is_on_the_prilo_login_page();
-        the_user_enters_a_valid_email_and_password("pablo183testy@gmail.com", "!Pablo183");
-    }
-    */
 
     @Given("The user is on the Prilo login page")
     public void the_user_is_on_the_prilo_login_page() {
@@ -33,31 +27,28 @@ public class LoginShipper {
 
     @Given("The user closes the cookie information popup")
     public void the_user_closes_the_cookie_information_popup() {
-        $(By.xpath("//body/app-root[1]/app-cookie-bar[1]/div[1]/div[1]/div[2]/p-button[1]/button[1]")).click();
-
+        $(By.cssSelector("p-button[styleclass='ok-button']")).click();
     }
 
-    @When("The user enters a valid email {string} and password {string}")
-    public void the_user_enters_a_valid_email_and_password(String email, String password) {
+    @When("The user enters a valid email {string}")
+    public void the_user_enters_a_valid_email(String email) {
         $(By.xpath("//input[@id='email-input']")).sendKeys(email);
-        $(By.xpath("/html[1]/body[1]/app-root[1]/div[1]/div[1]/main[1]/app-sign-in[1]/div[2]/div[1]/form[1]/adamp-input-password[1]/div[1]/p-password[1]/div[1]/input[1]")).sendKeys(password);
+    }
 
+    @When("The user enters a valid password {string}")
+    public void the_user_enters_a_valid_password(String password) {
+        $(By.cssSelector("input[type='password']")).sendKeys(password);
     }
 
     @When("The user clicks the login button")
     public void the_user_clicks_the_login_button() {
-        sleep(3000);
         $("[type='submit']").click();
-
-
     }
 
     @Then("The user is redirected to the homepage")
     public void the_user_is_redirected_to_the_homepage() {
-        SelenideElement element = $x("//app-section-header/div[1]/div[1]/div[1]/div[2]/span[1]/img[1]");
-        element.shouldBe(visible);
         sleep(3000);
-
+        SelenideElement element = $("img[src='assets/icons/avatar.svg']");
+        element.shouldBe(visible);
     }
-
 }
--- src/test/resources/features/login_to_shipper.feature
+++ src/test/resources/features/login_to_shipper.feature
@@ -3,6 +3,7 @@ Feature: Logging in to the Prilo platform
   Scenario: Successful login as Shipper
     Given The user is on the Prilo login page
     And The user closes the cookie information popup
-    When The user enters a valid email "pablo183testy@gmail.com" and password "!Pablo183"
+    When The user enters a valid email "pablo183testy@gmail.com"
+    And The user enters a valid password "!Pablo183"
     And The user clicks the login button
     Then The user is redirected to the homepage
